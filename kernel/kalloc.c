49b88ae115a3d589b157a3d31eefbb9f20ad8ffa
cpd <cpd@cpd-X450LD.(none)>
2022-04-10 22:04:03 +0800

    finished lab1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,21 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+// count the number of bytes of free memory
+int
+kfreecount(void)
+{
+  struct run *r;
+  int count=0;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  while(r){
+    count++;
+    r=r->next;
+  }
+  release(&kmem.lock);
+
+  return count*PGSIZE;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
 
   np->state = RUNNABLE;
 
+  np->mask=p->mask;
+
   release(&np->lock);
 
   return pid;
@@ -692,4 +694,16 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// count the number of process whose state is not UNUSED
+int
+proccount(void)
+{
+  struct proc *p;
+  int n=0;
+  for(p = proc; p < &proc[NPROC]; p++)
+    if(p->state!=UNUSED)
+      n++;
+  return n;
 }
